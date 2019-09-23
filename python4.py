93007f99f0e358a1561dd9c6f2e32d3af1d1062a
GewoonJoepert <36069585+GewoonJoepert@users.noreply.github.com>
2019-09-23 09:26:52 +0200

    main menu opgemaakt

--- python4.py
+++ python4.py
@@ -1,42 +1,52 @@
 import os, time
 
+
 def start_menu():
     naam = input("Hoe heet je? ")
-    print("=-="*11)
-    print("Welkom " + naam + " bij mijn overhoor programma!")
-    print("     Type O voor een overhoring")
-    print("     Type R om een vertaling weg te halen")
-    print("     Type T om een vertaling toe te voegen")
-    print("     Type L om de woordelijsten te zien")
-    print("     Type Q om af te sluiten")
-    print("=-="*11)
+    os.system('cls')
+    print("=-="*15)
+    print("| {:41} |".format("Welkom " + naam + " bij mijn overhoor programma!"))
+    print("| {:41} |".format("  Type O voor een overhoring"))
+    print("| {:41} |".format("  Type R om een vertaling weg te halen"))
+    print("| {:41} |".format("  Type T om een vertaling toe te voegen"))
+    print("| {:41} |".format("  Type L om de woordelijsten te zien"))
+    print("| {:41} |".format("  Type Q om af te sluiten"))
+    print("=-="*15)
+#print("| Woord: {:14} Vertaling: {:22} |".format(woord, vertaling))
 
-def engels_lijst(engels):
-    for key, value in engels.items():
+def print_woordenlijst(woordenlijst):
+    for key, value in woordenlijst.items():
         print(key + " - " + value)
 
-def duits_lijst(duits):
-    for key, value in duits.items():
-        print(key + " - " + value)
 
-def frans_lijst(frans):
-    for key, value in frans.items():
-        print(key + " - " + value)
+def print_woordenlijsten(woordenlijsten):
+    for lijst in woordenlijsten:
+        print_woordenlijst(lijst)
 
-def voeg_engels_woord_toe(engels):
+
+def vraag_en_voeg_woorden_toe(woordenlijst):
     woord = input("type een woord in ")
     vertaling = input("voer de vertaling in ")
-    engels[woord] = vertaling
+    woordenlijst[woord] = vertaling
 
-def voeg_frans_woord_toe(frans):
-    woord1 = input("type een woord in ")
-    vertaling1 = input("voer de vertaling in ")
-    frans[woord1] = vertaling1
 
-def voeg_duits_woord_toe(duits):
-    woord2 = input("type een woord in ")
-    vertaling2 = input("voer de vertaling in ")
-    duits[woord2] = vertaling2
+def remove_woord(woord):
+    del woord[input("vul het woord in ")]
+
+
+def overhoor_keuze(welke_o):
+    if welke_o == "en-nl":
+        print("1")
+    elif welke_o == "du-nl":
+        print("2")
+    elif welke_o == "fr-nl":
+        print("3")
+    elif welke_o == "q":
+        main()
+        os.system('cls')
+    else:
+        print("Kies een van de aangegeven woordenlijsten")
+
 
 def main():
     engels = {"apple": "appel"}
@@ -44,61 +54,55 @@ def main():
     duits = {"nein": "nee"}
     start_menu()
     programma =  True
+
     while programma:
         keuze =  input("Wat is je keuze? ").lower()
 
         if keuze == "o":
-            welke_o = input("welke woordenlijst wil je overhoord hebben ")
-            if welke_o == "en-nl":
-                print("1")
-            elif welke_o == "du-nl":
-                    print("2")
-            elif welke_o == "fr-nl":
-                        print("1")
-            elif welke_o == "q":
-                main()
-                os.system('cls')
-            else:
-                print("Kies een van de aangegeven woordenlijsten")
+            welke_o = input("welke woordenlijst wil je overhoord hebben? kies uit: en-nl, du-nl & fr-nl ")
+            overhoor_keuze(welke_o)
 
         elif keuze == "q":
             programma = False
             os.system('cls')
 
         elif keuze == "l":
-            print("=-="*6)
-            print("Engelse lijst:")
-            print("")
-            engels_lijst(engels)
-            print("=-="*6)
-
-            print("Franse lijst:")
-            print("")
-            frans_lijst(frans)
-            print("=-="*6)
-
-            print("Duitse lijst:")
-            print("")
-            duits_lijst(duits)
-            print("=-="*6)
+            print_woordenlijsten([engels, duits, frans])
 
         elif keuze == "t":
-            keuze_t = input("aan welke lijst wil je een vertaling toevoegen? kies uit: en-nl, du-nl, fr-nl ")
+            keuze_t = input("aan welke lijst wil je een vertaling toevoegen? kies uit: en-nl, du-nl & fr-nl ")
             if keuze_t == "en-nl":
-                voeg_engels_woord_toe(engels)
-                engels_lijst(engels)
-            elif keuze-t == "fr-nl":
-                    voeg_frans_woord_toe(frans)
-                    frans_lijst(frans)
+                vraag_en_voeg_woorden_toe(engels)
+                print_woordenlijst(engels)
+            elif keuze_t == "fr-nl":
+                vraag_en_voeg_woorden_toe(frans)
+                print_woordenlijst(frans)
             elif keuze_t == "du-nl":
-                    voeg_duits_woord_toe(duits)
-                    duits_lijst(duits)
+                vraag_en_voeg_woorden_toe(duits)
+                print_woordenlijst(duits)
             else:
                 print("voer een van de aangegeven lijsten in")
+                os.system('cls')
+
+        elif keuze == "r":
+            keuze_r = input("uit welke lijst wil je iets verwijderen? kies uit: en-nl, du-nl & fr-nl ")
+            if keuze_r == "en-nl":
+                print_woordenlijst(engels)
+                remove_woord(engels)
+                print_woordenlijst(engels)
+            elif keuze_r == "du-nl":
+                print_woordenlijst(duits)
+                remove_woord(duits)
+                print_woordenlijst(duits)
+            elif keuze_r == "fr-nl":
+                print_woordenlijst(frans)
+                remove_woord(frans)
+                print_woordenlijst(frans)
+            else:
+                print("voer een van de aangegeven lijsten in")
+                os.system('cls')
 
         else:
             print("voer een van de aangegeven letters in!")
-
-
 if __name__ == "__main__":
     main()
